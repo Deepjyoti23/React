0763e427405723e8ff8360f97ed336fc5bb75bf2
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-08-12 00:30:28 +0530

    ..

--- usegelocate/src/App.js
+++ usegelocate/src/App.js
@@ -1,40 +1,48 @@
 import { useState } from "react";
+import { useGeolocation } from "./useGeolocation";
 
-function useGeolocation() {}
+// function useGeolocation() {}
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(false);
   const [countClicks, setCountClicks] = useState(0);
-  const [position, setPosition] = useState({});
-  const [error, setError] = useState(null);
+  // const { lat, lng } = position;
+  // const [isLoading, setIsLoading] = useState(fal se);
+  // const [position, setPosition] = useState({});
+  // const [error, setError] = useState(null);
+  const {isLoading,position:{lat,lng},error,getPosition} = useGeolocation();
 
-  const { lat, lng } = position;
-
-  function getPosition() {
+  function handleclick(){
     setCountClicks((count) => count + 1);
+    getPosition();
 
-    if (!navigator.geolocation)
-      return setError("Your browser does not support geolocation");
-
-    setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude
-        });
-        setIsLoading(false);
-      },
-      (error) => {
-        setError(error.message);
-        setIsLoading(false);
-      }
-    );
   }
+
+
+  // function getPosition() {
+  //   setCountClicks((count) => count + 1);
+
+  //   if (!navigator.geolocation)
+  //     return setError("Your browser does not support geolocation");
+
+  //   setIsLoading(true);
+  //   navigator.geolocation.getCurrentPosition(
+  //     (pos) => {
+  //       setPosition({
+  //         lat: pos.coords.latitude,
+  //         lng: pos.coords.longitude
+  //       });
+  //       setIsLoading(false);
+  //     },
+  //     (error) => {
+  //       setError(error.message);
+  //       setIsLoading(false);
+  //     }
+  //   );
+  // }
 
   return (
     <div>
-      <button onClick={getPosition} disabled={isLoading}>
+      <button onClick={handleclick} disabled={isLoading}>
         Get my position
       </button>
 
